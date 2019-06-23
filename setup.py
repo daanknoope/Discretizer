1b39cccd1df6f066daf17ddc8079d23afb3757e3
Daan Knoope <daan@knoope.dev>
2019-06-23 09:37:01 +0200

    Init repo

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='Discretizer',
+      version='0.1',
+      description='Discretization library for (D)BNs.',
+      author='Daan Knoope',
+      author_email='daan@knoope.dev',
+      license='MIT',
+      packages=find_packages(), install_requires=['pandas'],
+      python_requires='>3.6'
+      )
